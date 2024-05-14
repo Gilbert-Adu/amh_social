c87cd8bca750ab865602472ed6bdfc60bb78b57d
Gilbert Adu <gea33@cornell.edu>
2024-05-13 21:14:27 -0400

    more added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.render('home');
+});
+app.post('/dashboard', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email);
+    res.render('dashboard');
 });
 app.listen(3000, () => {
     console.log("up on 3000!")
